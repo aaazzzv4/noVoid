ed1ac5e821b4fefe3ae58db645fc163ca6df4b73
Aaron Salyer <aaron_salyer@yahoo.com>
2018-05-16 05:55:17 -0400

    Populating from most current version.

---
+++ src/ImagicalGamer/noVoid/Main.php
@@ -0,0 +1,36 @@
+<?php
+namespace ImagicalGamer\NoVoid;
+    use pocketmine\plugin\PluginBase;
+    use pocketmine\event\Listener;
+    use pocketmine\level\Position;
+    use pocketmine\level\Level;
+    use pocketmine\Player;
+    use pocketmine\entity\Entity;
+    use pocketmine\math\Vector3;
+    use pocketmine\event\player\PlayerMoveEvent;
+    
+class Main extends PluginBase implements Listener{
+    
+public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+}
+
+public function checkVoid(PlayerMoveEvent $event){
+    $player = $event->getPlayer();
+    $x = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getFloorX();
+    $y = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getFloorY();
+    $z = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getFloorZ();
+    $level = $this->getServer()->getDefaultLevel();
+        if($event->getTo()->getFloorY() < 0){
+            switch(mt_rand(1, 2) == 1){
+              case 1:
+              $player->teleport(new Position($x, $y, $z, $level));
+              $player->setHealth($player->getHealth(20));
+              $player->sendMessage("§d§l»§r§aYou were saved from the §eVOID §d§l«");
+              break;
+              case 2:
+              break;
+             }
+         }
+      }
+}
